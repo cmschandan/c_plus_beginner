6f9c384776dc33938b8e1d517699954f928f022c
Chandan Kumar <cmschandan>
2022-10-19 13:09:12 +0530

    first commit

---
+++ variable-scope.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int glo =6;
+void sum(){
+    int a;
+    cout<< glo;
+}
+
+int main(){
+    int glo = 9;
+    glo = 78;
+    int a = 4;
+    int b = 5;
+    float pi = 3.14;
+    sum();
+    cout <<glo;
+    return 0;
+}
